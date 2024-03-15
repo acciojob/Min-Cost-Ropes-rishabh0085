312b3586e815d6f2db8528ba5c7733fb4c12136e
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-15 09:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-class Solution {
-    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-        int distanceValue = 0;
-        boolean isValid;
-
-        // Iterate through each element in arr1
-        for (int value1 : arr1) {
-            isValid = true; // Assume current value1 is valid
-            
-            // Check against all elements in arr2
-            for (int value2 : arr2) {
-                // If the absolute difference is less than d, value1 is not valid
-                if (Math.abs(value1 - value2) <= d) {
-                    isValid = false;
-                    break; // No need to check other elements in arr2 for this value1
-                }
-            }
-            
-            // If value1 is valid, increment the distance value
-            if (isValid) {
-                distanceValue++;
-            }
-        }
-
-        return distanceValue;
+function mincost(arr)
+{
+  //write your code here
+  
+  arr.sort((a,b)=>a-b)
+   
+ let cost = 0
+    //console.log(arr)
+  
+  while(arr.length>1){
+       
+ let first = arr.shift()
+       
+ let second = arr.shift()
+       
+ cost+=first+second
+     
+   arr.push(first+second)
+      
+  arr.sort((a,b)=>a-b)
+      //  console.log(arr)
     }
+  
+  return cost
 }
+module.exports=mincost;
